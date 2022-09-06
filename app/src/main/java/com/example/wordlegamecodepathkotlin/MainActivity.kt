c8a1321e5b59ce38330c0f31798fce83db3bbff0
barshachaudhary <barsha.chaudhary@bison.howard.edu>
2022-09-05 23:33:03 -0400

    Cleaning up code

--- app/src/main/java/com/example/wordlegamecodepathkotlin/MainActivity.kt
+++ app/src/main/java/com/example/wordlegamecodepathkotlin/MainActivity.kt
@@ -31,13 +31,14 @@ class MainActivity : AppCompatActivity() {
         var wordToGuess = FourLetterWordList.getRandomFourLetterWord()
         fun scorekeeper(guess: String): Int {
             var score = 0
-            for ( i in 0..3) {
+            for (i in 0..3) {
                 if (guess[i] == wordToGuess[i]) {
-                    score ++
+                    score++
                 }
             }
             return score
         }
+
         fun checkGuess(guess: String): SpannableStringBuilder {
             val builder = SpannableStringBuilder()
             for (i in 0..3) {
@@ -57,9 +58,9 @@ class MainActivity : AppCompatActivity() {
             }
             return builder
         }
+
         var firstguess = true
         var x = 0
-//        val wordToGuess = FourLetterWordList.getRandomFourLetterWord()
         val restart: Button = findViewById<Button>(R.id.btnRestart)
         if (firstguess) {
             findViewById<Button>(R.id.btnCheck).setOnClickListener {
@@ -67,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                 val etInputWord: EditText = findViewById(R.id.etEnteredWord)
                 val inputWord = etInputWord.text.toString()
                 if (isAlpha(inputWord) and (inputWord.length == 4)) {
-                    Log.i("MainActivity", isAlpha(inputWord).toString() + " check");
+                    Log.i("MainActivity", isAlpha(inputWord).toString() + " check")
                     val input1: TextView = findViewById(R.id.tvFirstGuessed)
                     val status1: TextView = findViewById(R.id.tvFirstStatus)
                     val check1: TextView = findViewById(R.id.tvFirstCheck)
@@ -98,13 +99,17 @@ class MainActivity : AppCompatActivity() {
                         input2.isVisible = true
                         var status2: TextView = findViewById(R.id.tvSecondStatus)
                         status2.isVisible = true
-                        check2.setText(checkGuess(inputWord1.uppercase()), TextView.BufferType.SPANNABLE)
+                        check2.setText(
+                            checkGuess(inputWord1.uppercase()),
+                            TextView.BufferType.SPANNABLE
+                        )
                         check2.isVisible = true
                         statuscheck2.isVisible = true
                         etInputWord.setText("")
                         etInputWord.hideKeyboard()
-                    }else {
-                        Toast.makeText(this, "You can only enter 4 letters", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "You can only enter 4 letters", Toast.LENGTH_SHORT)
+                            .show()
 
                     }
                     findViewById<Button>(R.id.btnCheck).setOnClickListener {
@@ -117,7 +122,10 @@ class MainActivity : AppCompatActivity() {
                             input3.isVisible = true
                             var status3: TextView = findViewById(R.id.tvThirdStatus)
                             status3.isVisible = true
-                            check3.setText(checkGuess(inputWord.uppercase()), TextView.BufferType.SPANNABLE)
+                            check3.setText(
+                                checkGuess(inputWord.uppercase()),
+                                TextView.BufferType.SPANNABLE
+                            )
                             check3.isVisible = true
                             var statuscheck3: TextView = findViewById(R.id.tvThirdStatusCheck)
                             statuscheck3.isVisible = true
@@ -126,8 +134,9 @@ class MainActivity : AppCompatActivity() {
                             actualword.isVisible = true
                             etInputWord.setText("")
                             etInputWord.hideKeyboard()
-                        } else{
-                            Toast.makeText(this, "You can only enter 4 letters", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "You can only enter 4 letters", Toast.LENGTH_SHORT)
+                                .show()
 
                         }
                         findViewById<Button>(R.id.btnCheck).isEnabled = false
@@ -138,13 +147,13 @@ class MainActivity : AppCompatActivity() {
                             finish()
                             startActivity(intent)
                         }
-                      }
                     }
                 }
-
             }
+
         }
     }
+}
 
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
